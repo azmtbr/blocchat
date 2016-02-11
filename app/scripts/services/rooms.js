6467b891d2f24385997a875618173e56688aa407
Jon Malis <jmalis@live.com>
2016-02-11 11:36:14 -0700

    hopefully fixed git error

---
+++ app/scripts/services/rooms.js
@@ -0,0 +1,19 @@
+(function() {
+  function Rooms($firebaseArray) {
+    var firebaseRef = new Firebase('https://blocchat-1105.firebaseio.com/rooms');
+    var rooms = $firebaseArray(firebaseRef.child('rooms'));
+
+    return {
+      all: rooms,
+      getMessages: function(roomId) {
+
+        var array = $firebaseArray(new Firebase('https://blocchat-1105.firebaseio.com/rooms/rooms/' + roomId + '/messages'));
+        return array;
+      },
+    }
+  }
+
+  angular
+    .module('blocChat')
+    .factory('Rooms', ['$firebaseArray', Rooms]);
+})();
